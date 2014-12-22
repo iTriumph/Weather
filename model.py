dbb486593d7615f8620b03cb3cc6f61019127ae0
Ma <laomafeima@gmail.com>
2014-12-22 21:37:33 +0800

    增加model

---
+++ model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ma'
+
+
+class Model:
+    def __init__(self):
+        pass
+
+    def query(self):
+        pass
+
+    def get(self):
+        pass
+
+    def get_field(self):
+        pass
+
+    def update(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def get_last_sql(self):
+        pass
+
+    def where(self):
+        pass
+
+    def order(self):
+        pass
+
+    def limit(self):
+        pass
+
+    def fetch_sql(self):
+        pass

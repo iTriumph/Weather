7a5b55c20bc0a4b0c31a87241716504300a5d105
Ma <laomafeima@gmail.com>
2014-12-28 22:34:34 +0800

    增加model

--- model.py
+++ model.py
@@ -6,9 +6,10 @@ __author__ = 'Ma'
 
 class Model:
     table_name = None
-    _where = None
+    _where = dict()
     _order = None
-    _limit = None
+    _limit_start = 0
+    _limit_length = 0
 
     def __init__(self):
         self.table_name = self.__class__.__name__
@@ -22,41 +23,50 @@ class Model:
     def get_field(self):
         pass
 
-    def update(self, data, where):
-        pass
+    def update(self, data):
+        print "UPDATE table SET " + ",".join([k + "=" + str(v) for k, v in data.items()])
 
     def insert(self, data):
-        pass
+        print data.values()
+        print ",".join([2,6,8])
+        sql = "INSERT INTO " + self.table_name + "(" + ",".join(data.keys()) + ") VALUES (" + ",".join(
+            data.values()) + ")"
+        return sql
 
-    def delete(self, where):
-        pass
+    def delete(self):
+        sql = "DELETE FROM " + self.table_name + self._build_sql()
 
     def get_last_sql(self):
         pass
 
     def where(self, where):
-        self._where = where
+        self._where.update(where)
         return self
 
     def order(self, column):
         self._order = column
         return self
 
-    def limit(self, limit):
-        self._limit = limit
+    def limit(self, start, length=0):
+        if length > 0:
+            self._limit_start = start
+            self._limit_length = length
+        else:
+            self._limit_start = 0
+            self._limit_length = start
         return self
 
     def fetch_sql(self):
-        self._build_sql()
+        return self._build_sql()
 
     def _build_sql(self):
-        #  生成 WHERE
+        # 生成 WHERE
         where_str = ""
         for i in self._where:
             column = i.strip()
             contrast = "="
             if column.find(":") >= 0:  # 处理对比操作符=,>,<,>=等
-                contrast = column[column.find(":")+1:]
+                contrast = column[column.find(":") + 1:]
                 column = column[0:column.find(":")]
 
             if column.startswith("&"):  # 处理 AND
@@ -72,12 +82,19 @@ class Model:
                     where_str += column
 
             where_str += contrast + str(self._where.get(i))
-
+        where_str = where_str.lstrip(" AND ")  # 去除开始的AND操作，因为如果有操作符，会排到字典的后面。
+        # 处理排序
         order_str = ",".join(self._order)
-        print order_str
-
+        # 拼装组合
+        sql = ""
+        if where_str:
+            sql += "WHERE " + where_str
+        if order_str:
+            sql += " ORDER BY " + order_str
+        if self._limit_length > 0:
+            sql += " LIMIT " + self._limit_start + "," + self._limit_length
+        return sql
 
 
 model = Model()
-print model.where({"name": "ma", "|age:>": 22}).order(['name','age']).fetch_sql()
-print model._where
+print model.insert({"name": "ma", "age": 22})

a2ad4a403f02c2ad00bc5afab9dc16b618e78df4
Ma <laomafeima@gmail.com>
2014-12-30 22:17:28 +0800

    增加model

--- index.py
+++ index.py
@@ -2,3 +2,20 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Ma'
+
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ model/user_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ma'
+
+
+class UserModel:
+    def __init__(self):
+        pass
